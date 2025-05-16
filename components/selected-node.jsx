0e21c94e1c692074c1c486eda71f297151725090
hmarcos8472 <realtimeoperatingsystem.97@gmail.com>
2025-05-15 17:51:57 -0700

    updated styling for selected node

--- components/selected-node.jsx
+++ components/selected-node.jsx
@@ -8,91 +8,117 @@ export default function SelectedNode({ node, handleLinkClick, deselectNode, grap
 
   return (
     <>
-      <div>
+      <div className="flex flex-col items-left w-[30vw] gap-1 z-20 fixed left-[1vw] top-[2vh]">
+        <div className="flex flex-row justify-between items-center">
+        <a href={node.url} className="text-gray-700 text-md p-0">{node.title}</a>
+        <div className="flex gap-1 items-center">
+          <select
+            onChange={(e) => setView(e.target.value)}
+            value={view}
+            className="px-4 py-2 text-xs cursor-pointer flex items-center w-[110px]"
+          >
+            <option value="summary">Summary</option>
+            <option value="details">Links</option>
+            <option value="image details">Image</option>
+          </select>
+
+          <Button
+            variant="ghost"
+            onClick={deselectNode}
+            className="h-[24px] rounded-full px-4 py-2 text-xs border bg-white"
+          >
+            Close
+          </Button>
+        </div>
+        </div>
+
         {view === "summary" && (
-          <div className="flex flex-col items-left max-h-[80vh] w-[30vw] gap-1 bg-gray-100 rounded-sm border py-[5px] px-[10px] backdrop-blur-md bg-opacity-50 z-20 fixed left-[1vw] top-[2vh]">
-            <a href={node.url} className="text-gray-700 text-s p-1">{node.title}</a>
-            <div className={`mt-0 max-h-[50vh] overflow-y-auto overflow-x-hidden !p-2 ${styles.scroll}`}>
-              <p className="text-xs p-1">{node.summary}</p>
-            </div>
-            <div className="flex gap-2">
-              <Button
-                variant="ghost"
-                onClick={() => setView("details")}
-                className="h-[24px] w-[50px] rounded-full px-4 py-2 text-xs border bg-white"
-              >
-                Details
-              </Button>
-              <Button
-                variant="ghost"
-                onClick={deselectNode}
-                className="h-[24px] w-[50px] rounded-full px-4 py-2 text-xs border bg-white"
-              >
-                Close
-              </Button>
+          <div className="flex flex-col items-left">
+            <div className={`mt-0 max-h-[450px] overflow-y-auto overflow-x-hidden !p-0 ${styles.scroll}`}>
+              {node.summary.split("\n\n").map((paragraph, index) => (
+                <p key={index} className="text-xs p-0">
+                  {paragraph}
+                </p>
+              ))}
             </div>
           </div>
         )}
 
         {view === "details" && (
-          <div className="flex flex-col items-left max-h-[80vh] w-[30vw] gap-1 bg-gray-100 rounded-sm border py-[5px] px-[10px] backdrop-blur-md bg-opacity-50 z-20 fixed left-[1vw] top-[2vh]">
-          <p className="text-gray-700 text-s p-1">{node.title}</p>
-          <div className={`mt-0 max-h-[50vh] overflow-y-auto overflow-x-hidden !p-2 ${styles.scroll}`}>
-            <p className="text-gray-700 text-s">Image Source</p>
-            <p className="text-teal-600 text-xs w-full overflow-ellipsis nowrap">
-              {node.image}
-            </p>
-            <div className="border-t border-gray-300 my-2"></div>
-            <p className="text-gray-700 text-s">Links</p>
-            <div className="mt-0">
-              {node.links && (
-                <div className="grid grid-cols-2 gap-2">
-                  {node.links.map((link, index) => {
-                    const linkedNode = graphData.nodes.find((n) => n.url === link.url);
+          <div>
+            <div className={`mt-0 max-h-[50vh] overflow-y-auto overflow-x-hidden !p-0 ${styles.scroll}`}>
 
-                    return (
-                      <div
-                        key={index}
-                        className="border p-1 flex flex-row justify-between items-center relative group cursor-pointer"
-                      >
-                        <p className="h-[18px] max-h-[18px] w-[full] text-gray-700 text-xs text-left overflow-ellipsis overflow-hidden">
-                          {link.title}
-                        </p>
-                        {linkedNode ? (
-                          <span>
-                            
-                          </span>
-                        ) : (
-                          <span
-                            className="cursor-pointer"
-                            onClick={() => handleLinkClick(link)}
-                          >
-                            +
-                          </span>
-                        )}
-                      </div>
-                    );
-                  })}
-                </div>
+              <div className="mt-0">
+                {node.links && (
+                  <div className="flex flex-col gap-0">
+                    {(() => {
+                      // Sort links by title length
+                      const sortedLinks = [...node.links].sort((a, b) => b.title.length - a.title.length);
+
+                      // Define the number of items per row for each row
+                      const itemsPerRowConfig = [2, 2, 3, 3, 2, 2, 1];
+                      // Group links into rows based on the configuration
+                      const rows = [];
+                      let index = 0;
+
+                      itemsPerRowConfig.forEach((itemsPerRow) => {
+                        if (index < sortedLinks.length) {
+                          rows.push(sortedLinks.slice(index, index + itemsPerRow));
+                          index += itemsPerRow;
+                        }
+                      });
+
+                      return rows.map((row, rowIndex) => (
+                        <div key={rowIndex} className="flex flex-row gap-0">
+                          {row.map((link, index) => {
+                            const linkedNode = graphData.nodes.find((n) => n.url === link.url);
+
+                            return (
+                              <div
+                                key={index}
+                                className="p-1 flex flex-row justify-between items-center cursor-pointer"
+                              >
+                                <p className=" max-h-[18px] text-gray-700 text-xs text-left overflow-ellipsis overflow-hidden">
+                                  {link.title}
+                                </p>
+                                {linkedNode ? (
+                                  <span></span>
+                                ) : (
+                                  <span
+                                    className="cursor-pointer"
+                                    onClick={() => handleLinkClick(link)}
+                                  >
+                                    
+                                  </span>
+                                )}
+                              </div>
+                            );
+                          })}
+                        </div>
+                      ));
+                    })()}
+                  </div>
+                )}
+              </div>
+            </div>
+
+          </div>
+        )}
+
+        {view === "image details" && (
+          <div>
+            <div className="flex flex-col items-center">
+              {node.image ? (
+                <Image
+                  src={node.image}
+                  alt={node.title}
+                  width={250}
+                  height={200}
+                />
+              ) : (
+                <p className="text-gray-500 text-xs">No image available</p>
               )}
             </div>
-          </div>
-          <div className="flex gap-2">
-              <Button
-                variant="ghost"
-                onClick={() => setView("summary")}
-                className="h-[24px] w-[70px] rounded-full px-4 py-2 text-xs border bg-white"
-              >
-                Summary
-              </Button>
-              <Button
-                variant="ghost"
-                onClick={deselectNode}
-                className="h-[24px] w-[50px] rounded-full px-4 py-2 text-xs border bg-white"
-              >
-                Close
-              </Button>
-          </div>
           </div>
         )}
       </div>
