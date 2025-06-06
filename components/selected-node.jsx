6d01cbe1bb4db75377ecac9177c0238103e0e9ea
hmarcos8472 <realtimeoperatingsystem.97@gmail.com>
2025-06-05 18:47:00 -0700

    update styling for selected node to match graphdata menu

--- components/selected-node.jsx
+++ components/selected-node.jsx
@@ -12,52 +12,50 @@ export default function SelectedNode({ node, handleLinkClick, deselectNode, grap
 
   return (
     <>
-      <div className={`flex flex-col items-left w-[30vw] gap-1 z-20 fixed left-[1vw] top-[2vh] max-h-[80vh] overflow-y-auto overflow-x-hidden !bg-gray-100 !rounded-md border backdrop-blur-md !bg-opacity-50 ${styles.scroll}`}>
-        <div className="flex flex-row justify-between items-center">
-          <a href={node.url} className="text-gray-700 text-md font-bold p-0">{node.title}</a>
-        
-          <div className="flex gap-1 items-center">
-            <Button
+      <div className={`flex flex-col items-left z-20 fixed left-[1vw] top-[2vh] w-[30vw] max-h-[80vh] overflow-y-auto overflow-x-hidden !bg-gray-100 !rounded-md border backdrop-blur-md !bg-opacity-50 ${styles.scroll}`}>
+        <div className="flex flex-row justify-between items-center px-2 py-1 border-b sticky top-0 bg-gray-100 bg-opacity-80 backdrop-blur-xl">
+          <div className="flex flex-col gap-0 p-0">
+          <a href={node.url} className="text-gray-700 text-sm font-bold p-0">{node.title}</a>
+          <div className="flex flex-row gap-1">
+            <div
+              onClick={() => setShowSummary(!showSummary)}
+              className={`text-[9px] cursor-pointer ${showSummary ? "text-blue-500" : "text-gray-700"}`}
+            >
+              Summary
+            </div>
+            <div
+              onClick={() => setShowDetails(!showDetails)}
+              className={`text-[9px] cursor-pointer ${showDetails ? "text-blue-500" : "text-gray-700"}`}
+            >
+              Links
+            </div>
+            <div
+              onClick={() => setShowImageDetails(!showImageDetails)}
+              className={`text-[9px] cursor-pointer ${showImageDetails ? "text-blue-500" : "text-gray-700"}`}
+            >
+              Image
+            </div>
+            <div
+              onClick={() => setShowEdges(!showEdges)}
+              className={`text-[9px] cursor-pointer ${showEdges ? "text-blue-500" : "text-gray-700"}`}
+            >
+              Edges
+            </div>
+          </div>
+          </div>
+          <Button
               variant="ghost"
               onClick={deselectNode}
               className="h-[24px] w-[24px] flex items-center justify-center"
               aria-label="Close"
             >
               <XMarkIcon className="h-4 w-4 text-black-700" />
-            </Button>
-          </div>
-        </div>
-
-        <div className="flex flex-row gap-4">
-          <div
-            onClick={() => setShowSummary(!showSummary)}
-            className={`text-xs cursor-pointer ${showSummary ? "text-blue-500" : "text-gray-700"}`}
-          >
-            Summary
-          </div>
-          <div
-            onClick={() => setShowDetails(!showDetails)}
-            className={`text-xs cursor-pointer ${showDetails ? "text-blue-500" : "text-gray-700"}`}
-          >
-            Links
-          </div>
-          <div
-            onClick={() => setShowImageDetails(!showImageDetails)}
-            className={`text-xs cursor-pointer ${showImageDetails ? "text-blue-500" : "text-gray-700"}`}
-          >
-            Image
-          </div>
-          <div
-            onClick={() => setShowEdges(!showEdges)}
-            className={`text-xs cursor-pointer ${showEdges ? "text-blue-500" : "text-gray-700"}`}
-          >
-            Edges
-          </div>
+          </Button>
         </div>
 
         <div className="flex flex-col gap-4">
           {showSummary && (
-            <div className="flex flex-col items-left border-t">
+            <div className="flex flex-col items-left border-t px-3 mb-2">
               <h3 className="text-sm mt-2">Summary</h3>
               <div className="mt-0 !p-0">
                 {node.summary.split("\n\n").map((paragraph, index) => (
@@ -70,7 +68,7 @@ export default function SelectedNode({ node, handleLinkClick, deselectNode, grap
           )}
 
           {showDetails && (
-            <div className="border-t">
+            <div className="border-t px-3 mb-2">
               <h3 className="text-sm mt-2">Links</h3>
               <div className="mt-0 max-h-[50vh] overflow-y-auto overflow-x-hidden !p-0">
                 <div>
@@ -121,7 +119,7 @@ export default function SelectedNode({ node, handleLinkClick, deselectNode, grap
           )}
 
           {showImageDetails && (
-            <div className="border-t">
+            <div className="border-t px-3 mb-2">
               <h3 className="text-sm mt-2">Image</h3>
               <div className="flex flex-col items-center">
                 {node.image ? (
@@ -139,7 +137,7 @@ export default function SelectedNode({ node, handleLinkClick, deselectNode, grap
           )}
 
           {showEdges && (
-            <div className="border-t">
+            <div className="border-t px-3 mb-2">
               <h3 className="text-sm mt-2">Edges</h3>
               <table className="table-auto border-collapse border border-gray-300 text-xs max-w-full">
                 <thead>
